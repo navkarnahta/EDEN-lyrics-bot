9d8e17b4c0617c17d11d00e2360adc8954a3b2ef
navkarnahta <89142361+navkarnahta@users.noreply.github.com>
2021-08-19 19:32:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,24 +6,17 @@ autho = {'consumer_key' : 'a' ,
          'consumer_secret' : 'b',
          'key' : 'b',
          'secret' : 'c'}
+
 def get_tkey(): #module to acquire the twitter consumer key,secret key, API key and API secret key from the text file
     with open("tkey.txt","r") as f:
         lines = f.read()
         return lines
+
 def get_gkey(): #module to acquire the genius access token from the text file
     with open("gkey.txt","r") as f:
         lines = f.readlines()
         return lines[0].strip()
-s = get_tkey()
-lines = s.split(',')
-c=0
-for i in autho:
-    autho.update({i:str(lines[c])})
-    c+=1
-auth = tweepy.OAuthHandler(autho['consumer_key'], autho['consumer_secret'])
-auth.set_access_token(autho['key'], autho['secret'])
-#manually added the songs to the list after searching them up using the artist.songs command
-songs = ["XO","​drugs","Wake Up","​sex (catching feelings)", "​rock + roll", "Gravity", "​crash", "Fumes", "​start//end", "Circles", "​forever//over", "​gold", "​love; not wrong (brave)", "​take care", "​wings", "​and", "​isohel", "​icarus", "Nocturne", "​falling in reverse","​float", "​wonder", "​909", "​projector","​love, death, distraction", "​wrong", "​lost//found", "​untitled", "​catch me if you can", "Interlude", "​hertz", "​nowhere else", "​just saying", "2020", "​how to sleep", "​good morning","​fomo", "????", "​about time​", "Peaked", "​rushing", "​so far so good", "02 09", "​calm down", "$treams", "​re//start", "​in", "​tides", "​static", "​out", "Amnesia", "Hey Ya", "​hello", "​running, tripping, falling (no future)", "XO (Extended)", "​all i want", "​call me back*","Dreaming About You*"]
+
 def get_raw_lyrics():
    genius_client_access_token = str(get_gkey())
    genius = lyricsgenius.Genius(genius_client_access_token)
@@ -31,6 +24,7 @@ def get_raw_lyrics():
    lyrics = genius.search_song(rand_song_title, "EDEN").lyrics.lower()
    song = rand_song_title.lower() + " - EDEN"
    return lyrics, song
+
 def get_tweet_from(lyrics):
    lines = lyrics.split('\n')
    for i in range(len(lines)):
@@ -42,7 +36,26 @@ def get_tweet_from(lyrics):
    tweet = tweet.replace("\\", "")
    return tweet
 
-api = tweepy.API(auth)
-lyrics, song = get_raw_lyrics()
-tweet = get_tweet_from(lyrics)
-api.update_status(tweet+"\n\n"+song)
+def tweetIt():
+         api = tweepy.API(auth)
+         try:
+                  api.verify_credentials()
+                  print("Authentication OK")
+         except:
+                  print("Error during authentication")
+         lyrics, song = get_raw_lyrics()
+         tweet = get_tweet_from(lyrics)
+         api.update_status(tweet+"\n\n"+song)
+
+s = get_tkey()
+lines = s.split(',')
+c=0
+for i in autho:
+    autho.update({i:str(lines[c])})
+    c+=1
+auth = tweepy.OAuthHandler(autho['consumer_key'], autho['consumer_secret'])
+auth.set_access_token(autho['key'], autho['secret'])
+#manually added the songs to the list after searching them up using the artist.songs command
+songs = ["XO","drugs","Wake Up","sex (catching feelings)", "rock + roll", "Gravity", "crash", "Fumes", "start//end", "Circles", "forever//over", "gold", "love; not wrong (brave)", "take care", "wings", "and", "isohel", "icarus", "Nocturne", "falling in reverse","float", "wonder", "909", "projector","love, death, distraction", "wrong", "lost//found", "untitled", "catch me if you can", "Interlude", "hertz", "nowhere else", "just saying", "2020", "how to sleep", "good morning","fomo", "????", "about time", "Peaked", "rushing", "so far so good", "02 09", "calm down", "$treams", "re//start", "in", "tides", "static", "out", "Amnesia", "Hey Ya", "hello", "running, tripping, falling (no future)", "XO (Extended)", "all i want", "call me back*","Dreaming About You"]
+tweetIt()
+time.sleep()

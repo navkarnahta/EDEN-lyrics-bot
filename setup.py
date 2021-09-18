79abfa5b2dba0c535960ae4d82b3010e74fef672
navkarnahta <89142361+navkarnahta@users.noreply.github.com>
2021-09-19 00:38:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EDEN-lyrics-bot',
-    version='1.0.0',
+    version='1.0.2',
     description='Lyrics Generation Bot',
     author='Navkar Nahta',
     author_email='navkar.nahta@gmail.com',

cd65e8ae97a7ba0a33e1fe4aeceddf8f011c11bd
navkarnahta <89142361+navkarnahta@users.noreply.github.com>
2021-09-19 00:30:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
+import time
 import tweepy
 import random
 import lyricsgenius
 
 autho = {'consumer_key' : 'a' , 
-         'consumer_secret' : 'b',
-         'key' : 'b',
-         'secret' : 'c'}
-
-def get_tkey(): #module to acquire the twitter consumer key,secret key, API key and API secret key from the text file
+        'consumer_secret' : 'b',
+        'key' : 'b',
+        'secret' : 'c'}
+def get_tkey():
     with open("tkey.txt","r") as f:
         lines = f.read()
         return lines
-
-def get_gkey(): #module to acquire the genius access token from the text file
+def get_gkey():
     with open("gkey.txt","r") as f:
         lines = f.readlines()
         return lines[0].strip()
@@ -24,7 +23,6 @@ def get_raw_lyrics():
    lyrics = genius.search_song(rand_song_title, "EDEN").lyrics.lower()
    song = rand_song_title.lower() + " - EDEN"
    return lyrics, song
-
 def get_tweet_from(lyrics):
    lines = lyrics.split('\n')
    for i in range(len(lines)):
@@ -32,6 +30,10 @@ def get_tweet_from(lyrics):
            lines[i] = "XXX"
    lines = [i for i in lines if i != "XXX"]
    rand_num = random.randrange(0, len(lines)-1)
+   linep1 = lines[rand_num+1]
+   if linep1.find("embedshare urlcopyembedcopy")==True:
+       linep1.replace("embedshare urlcopyembedcopy","")
+   tweet = lines[rand_num] + "\n" + linep1 + " "
    tweet = lines[rand_num] + "\n" + lines[rand_num+1]
    tweet = tweet.replace("\\", "")
    return tweet
@@ -55,7 +57,6 @@ for i in autho:
     c+=1
 auth = tweepy.OAuthHandler(autho['consumer_key'], autho['consumer_secret'])
 auth.set_access_token(autho['key'], autho['secret'])
-#manually added the songs to the list after searching them up using the artist.songs command
-songs = ["XO","drugs","Wake Up","sex (catching feelings)", "rock + roll", "Gravity", "crash", "Fumes", "start//end", "Circles", "forever//over", "gold", "love; not wrong (brave)", "take care", "wings", "and", "isohel", "icarus", "Nocturne", "falling in reverse","float", "wonder", "909", "projector","love, death, distraction", "wrong", "lost//found", "untitled", "catch me if you can", "Interlude", "hertz", "nowhere else", "just saying", "2020", "how to sleep", "good morning","fomo", "????", "about time", "Peaked", "rushing", "so far so good", "02 09", "calm down", "$treams", "re//start", "in", "tides", "static", "out", "Amnesia", "Hey Ya", "hello", "running, tripping, falling (no future)", "XO (Extended)", "all i want", "call me back*","Dreaming About You"]
+songs = ["XO","​drugs","Wake Up","​sex (catching feelings)", "​rock + roll", "Gravity", "​crash", "Fumes", "​start//end", "Circles", "​forever//over", "​gold", "​love; not wrong (brave)", "​take care", "​wings", "​and", "​isohel", "​icarus", "Nocturne", "​falling in reverse","​float", "​wonder", "​909", "​projector","​love, death, distraction", "​wrong", "​lost//found", "​untitled", "​catch me if you can", "Interlude", "​hertz", "​nowhere else", "​just saying", "2020", "​how to sleep", "​good morning","​fomo", "​about time​", "Peaked", "​rushing", "​so far so good", "02 09", "​calm down", "$treams", "​re//start", "​in", "​tides", "​static", "​out", "Amnesia", "Hey Ya", "​hello", "​running, tripping, falling (no future)", "XO (Extended)", "​all i want", "​call me back*","Dreaming About You*"]
 tweetIt()
-time.sleep()
+time.sleep(16200)

3894d88786b73064096a8f1001cc3b97f6b5e099
navkarnahta <89142361+navkarnahta@users.noreply.github.com>
2021-10-21 19:11:12 +0530

    updated the tweet format

--- bot.py
+++ bot.py
@@ -7,18 +7,15 @@ autho = {'consumer_key': 'a',
          'key': 'b',
          'secret': 'c'}
 
-
 def get_tkey():
     with open("tkey.txt", "r") as f:
         lines = f.read()
         return lines
 
-
 def get_gkey():
     with open("gkey.txt", "r") as f:
         lines = f.readlines()
         return lines[0].strip()
-
 
 def get_raw_lyrics():
     genius_client_access_token = str(get_gkey())
@@ -27,7 +24,6 @@ def get_raw_lyrics():
     lyrics = genius.search_song(rand_song_title, "EDEN").lyrics.lower()
     song = rand_song_title.lower() + " - EDEN"
     return lyrics, song
-
 
 def get_tweet_from(lyrics):
     lines = lyrics.split('\n')
@@ -40,11 +36,9 @@ def get_tweet_from(lyrics):
     if linep1.find(r"[0-9]*embedshare URLCopyEmbedCopy") == True:
         linep1.replace(r"[0-100]*embedshare URLCopyEmbedCopy"," ")
     tweet = lines[rand_num] + "\n" + linep1 + " "
-    if tweet.find('''"''')== True:
-        tweet.replace(''''"''',''' ''')
+    tweet.replace(''''"''',''' ''')
     tweet = tweet.replace("\\", "")
     return tweet
-
 
 def tweetIt():
     api = tweepy.API(auth)
@@ -55,8 +49,10 @@ def tweetIt():
         print("Error during authentication")
     lyrics, song = get_raw_lyrics()
     tweet = get_tweet_from(lyrics)
-    api.update_status(tweet+"\n\n"+song)
-
+    t = api.update_status(tweet)
+    id = t.id
+    in_reply_to_status_id = str(id)
+    api.update_status(song,in_reply_to_status_id)
 
 s = get_tkey()
 lines = s.split(',')
@@ -64,8 +60,9 @@ c = 0
 for i in autho:
     autho.update({i: str(lines[c])})
     c += 1
- 
 auth = tweepy.OAuthHandler(autho['consumer_key'], autho['consumer_secret'])
 auth.set_access_token(autho['key'], autho['secret'])
-songs = ["XO", "​drugs", "Wake Up", "​sex (catching feelings)", "​rock + roll", "Gravity", "​crash", "Fumes", "​start//end", "Circles", "​forever//over", "​gold", "​love; not   wrong (brave)", "​take care", "​wings", "​and", "​isohel", "​icarus", "Nocturne", "​falling in reverse", "​float", "​wonder", "​909", "​projector", "​love, death, distraction", "​wrong", "​lost//found", "​untitled", "​catch me if you can", "Interlude", "​hertz", "​nowhere else", "​just saying", "2020", "​how to sleep", "​good morning", "​fomo", "​about time​", "Peaked", "​rushing", "​so far so good", "02:09", "​calm down", "$treams", "​re//start", "​in", "​tides", "​static", "​out", "Amnesia", "Hey Ya", "​hello", "​running, tripping, falling (no future)", "XO (Extended)", "​all i want", "​call me back", "Dreaming About You"]
+songs = ["XO", "​drugs", "Wake Up", "​sex (catching feelings)", "​rock + roll", "Gravity", "​crash", "Fumes", "​start//end", "Circles", "​forever//over", "​gold", "​love; not wrong (brave)", "​take care", "​wings", "​and", "​isohel", "​icarus", "Nocturne", "​falling in reverse", "​float", "​wonder", "​909", "​projector", "​love, death, distraction", "​wrong", "​lost//found", "​untitled", "​catch me if you can",
+         "Interlude", "​hertz", "​nowhere else", "​just saying", "2020", "​how to sleep", "​good morning", "​fomo", "​about time​", "Peaked", "​rushing", "​so far so good", "02:09", "​calm down", "$treams", "​re//start", "​in", "​tides", "​static", "​out", "Amnesia", "Hey Ya", "​hello", "​running, tripping, falling (no future)", "XO (Extended)", "​all i want", "​call me back", "Dreaming About You"]
 tweetIt()
+exit()
